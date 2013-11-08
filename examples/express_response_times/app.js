73bf1ed664180f85a21181dbc660f3d4ff72dd66
Ben Evans <ben@bensbit.co.uk>
2013-11-08 18:20:56 +0000

    Example Express response times

---
+++ examples/express_response_times/app.js
@@ -0,0 +1,70 @@
+
+var express = require('express');
+var http    = require('http');
+var influx  = require('../../');
+
+var app          = express();
+var serverInflux = influx();
+var dbInflux     = influx('localhost', 8086, 'example_response_dbuser', 'P85sw0rD', 'example_response');
+
+app.use(express.logger('dev'));
+
+/**
+ * LOOKING FOR JUST THE MIDDLEWARE?
+ *
+ *    THEN COPY THIS PART
+ *
+ *  WARNING: CHANGE THE ERROR THROW
+ */
+
+// TODO: Wait and then send multiple points within suitable timeframe.
+
+app.use(function(req, res, next) {
+  var startTime = new Date();
+  function logRequest(){
+    res.removeListener('finish', logRequest);
+    res.removeListener('close', logRequest);
+    dbInflux.writePoint('response_times', { time: startTime, value: (new Date()) - startTime }, function(err) {
+      if(err) throw err;
+    });
+  }
+  res.on('finish', logRequest);
+  res.on('close', logRequest);
+  return next();
+});
+
+/**
+ *      AND STOP HERE
+ */
+
+
+app.get('/', function(req, res) {
+  setTimeout(function() {
+    res.send('Good Day.');
+  }, Math.random() * 500);
+});
+
+
+function startApp() {
+  http.createServer(app).listen(3000, function() {
+    console.log('Listening on port 3000');
+  });
+}
+
+console.log('Checking DB Exists');
+serverInflux.getDatabaseNames(function(err, dbs) {
+  if(err) throw err;
+  if(dbs.indexOf('example_response') === -1) {
+    console.log('Creating Database');
+    serverInflux.createDatabase('example_response', function(err) {
+      if(err) throw err;
+      console.log('Creating User');
+      serverInflux.createUser('example_response', 'example_response_dbuser', 'P85sw0rD', function(err) {
+        if(err) throw err;
+        startApp();
+      });
+    });
+  } else {
+    startApp();
+  }
+});
