2ad16c158474feff760854218d016cf4c1d5d927
Ben Evans <ben@bluechimp.io>
2019-05-15 10:57:15 +0100

    fix: pollyfill blocking install

--- src/pool.ts
+++ src/pool.ts
@@ -318,7 +318,7 @@ export class Pool {
             // the request options, wrapped in a conditional for even worse
             // polyfills. See: https://github.com/node-influx/node-influx/issues/221
             if (typeof req.setTimeout === 'function') {
-              req.setTimeout(timeout, fail); // tslint:disable-line
+              req.setTimeout(timeout, () => {fail.call(this, arguments)}); // tslint:disable-line
             }
 
             req.on('timeout', fail);
