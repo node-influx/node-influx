1a1487978374427b8e4b9f337882a1a4eb474e68
Xabier Napal <xabiernapal@pm.me>
2021-01-29 12:16:23 +0100

    feat: add constant backoff strategy

--- test/unit/backoff.test.ts
+++ test/unit/backoff.test.ts
@@ -3,9 +3,41 @@
 import { expect } from "chai";
 
 import { IBackoffStrategy } from "../../src/backoff/backoff";
+import { ConstantBackoff } from "../../src/backoff/constant";
 import { ExponentialBackoff } from "../../src/backoff/exponential";
 
 describe("backoff strategies", () => {
+  describe("constant strategy", () => {
+    it("appears to work", () => {
+      let exp: IBackoffStrategy = new ConstantBackoff({
+        delay: 500,
+        jitter: 0.5,
+      });
+
+      function next(): number {
+        const value = exp.getDelay();
+        exp = exp.next();
+        return value;
+      }
+
+      const checkSequence = (): void => {
+        expect(next()).to.be.within(500 * (1 - 0.5), 500 * (1 + 0.5));
+        expect(next()).to.be.within(500 * (1 - 0.5), 500 * (1 + 0.5));
+        expect(next()).to.be.within(500 * (1 - 0.5), 500 * (1 + 0.5));
+        expect(next()).to.be.within(500 * (1 - 0.5), 500 * (1 + 0.5));
+        expect(next()).to.be.within(500 * (1 - 0.5), 500 * (1 + 0.5));
+        expect(next()).to.be.within(500 * (1 - 0.5), 500 * (1 + 0.5));
+      };
+
+      checkSequence();
+      exp = exp.reset();
+      const dupe = exp.reset();
+      checkSequence();
+      exp = dupe;
+      checkSequence();
+    });
+  });
+
   describe("exponential strategy", () => {
     it("appears to work", () => {
       let exp: IBackoffStrategy = new ExponentialBackoff({
