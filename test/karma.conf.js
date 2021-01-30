5be5eb772e5034863fb438c19fe52070b20188e2
Ben Evans <ben@bluechimp.io>
2021-01-30 16:40:07 +0000

    ci: run browser tests on chrome and upload coverage

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,30 +1,5 @@
 const path = require("path");
-const isSaucy = !!process.env.SAUCE;
-const sauceLaunchers = {
-  sauceChrome: {
-    base: "SauceLabs",
-    browserName: "chrome",
-    platform: "Windows 10",
-    version: "54.0",
-  },
-  sauceFirefox: {
-    base: "SauceLabs",
-    browserName: "firefox",
-    platform: "Windows 10",
-    version: "49.0",
-  },
-  sauceEdge: {
-    base: "SauceLabs",
-    browserName: "MicrosoftEdge",
-    platform: "Windows 10",
-    version: "14.14393",
-  },
-};
-
-if (/^[0-9]+$/.test(process.env.TRAVIS_PULL_REQUEST)) {
-  console.log("Refusing to run SauceLabs tests in pull requests");
-  process.exit(0);
-}
+process.env.CHROME_BIN = require('puppeteer').executablePath()
 
 module.exports = function (config) {
   config.set({
@@ -33,7 +8,7 @@ module.exports = function (config) {
      */
     basePath: path.join(__dirname, ".."),
     frameworks: ["mocha"],
-    reporters: isSaucy ? ["mocha", "saucelabs"] : ["mocha"],
+    reporters: ['mocha'],
 
     client: {
       mocha: {
@@ -67,20 +42,9 @@ module.exports = function (config) {
     middleware: ["pool-tests"],
 
     /**
-     * SauceLabs config:
-     */
-    sauceLabs: {
-      testName: "node-influx",
-      public: "public",
-      tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER,
-      startConnect: false,
-    },
-    customLaunchers: sauceLaunchers,
-
-    /**
      * Karma run config:
      */
-    browsers: isSaucy ? Object.keys(sauceLaunchers) : ["Chrome"],
+    browsers: ['ChromeHeadless'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
